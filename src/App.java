15d530aa5a687f109935d967b88aa70cc8a9900d
Kaik Mendes <kaik.eu19@hotmail.com>
2023-08-30 10:17:16 -0300

    veiculo teste

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         veiculo car = new veiculo();
+        car.velocidade = 100;
+        car.cor = "azul";
+        car.modelo = "monza";
         car.buzinar();
+
+        System.out.println("A velocidade é " + car.velocidade);
     }
 }
