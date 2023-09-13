ace747424f7dc032d84f6431abbca1d66851f2aa
Kaik <kaik.eu19@hotmail.com>
2023-09-13 10:49:46 -0300

    corpo humano commit

--- src/App.java
+++ src/App.java
@@ -8,5 +8,12 @@ public class App {
 
         int velocidade = car.acelerar(100, 150);
         System.out.println("A velocidade é " + velocidade);
+
+        corpoHumano corpo1 = new corpoHumano();
+        corpo1.massa = 80;
+        corpo1.volume = 10;
+        
+        corpo1.densidade = corpo1.massa/corpo1.volume;
+        System.out.println("A densidade do corpo é: "+corpo1.densidade)
     }
 }
---
+++ src/corpoHumano.java
@@ -0,0 +1,5 @@
+public class corpoHumano{
+    public float massa;
+    public float densidade;
+    public float volume;
+}
